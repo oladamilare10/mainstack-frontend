3e40f6b58a9dbe2e83dba47228833a130664356d
Oluwadamilare Babalola <153187401+diamondnunstraw@users.noreply.github.com>
2025-10-19 22:15:11 -0700

    mainstack Debut

---
+++ src/types/react-icons.d.ts
@@ -0,0 +1,38 @@
+declare module 'react-icons/md' {
+  import { IconType } from 'react-icons';
+  export const MdClose: IconType;
+  export const MdKeyboardArrowDown: IconType;
+}
+
+declare module 'react-icons/bi' {
+  import { IconType } from 'react-icons';
+  export const BiChevronDown: IconType;
+  export const BsArrowDownLeft: IconType;
+}
+
+declare module 'react-icons/bs' {
+  import { IconType } from 'react-icons';
+  export const BsArrowDownLeft: IconType;
+}
+
+declare module 'react-icons/io5' {
+  import { IconType } from 'react-icons';
+  export const IoNotificationsOutline: IconType;
+  export const IoChatbubbleEllipsesOutline: IconType;
+  export const IoChevronDown: IconType;
+}
+
+declare module 'react-icons/hi' {
+  import { IconType } from 'react-icons';
+  export const HiOutlineMenu: IconType;
+}
+
+declare module 'react-icons/fi' {
+  import { IconType } from 'react-icons';
+  export const FiDownload: IconType;
+}
+
+declare module 'react-icons/fa' {
+  import { IconType } from 'react-icons';
+  export const FaChevronDown: IconType;
+}
